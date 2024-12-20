395f860935092600e8299bab2161eb7b6a8f77da
ivan <ibodnar.dev@gmail.com>
2024-12-20 09:00:31 -0300

    settings updated

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,25 @@
+from functools import cached_property
+
 from pydantic_settings import BaseSettings
+from sqlalchemy import URL
 
 
 class Settings(BaseSettings):
+    postgres_server: str
+    postgres_port: int
     postgres_db: str
+    postgres_user: str
+    postgres_password: str
+
+    @cached_property
+    def postgres_url(self):
+        return URL.create(
+            "postgresql+psycopg2",
+            username=self.postgres_user,
+            password=self.postgres_password,
+            host=self.postgres_host,
+            database=self.postgres_db
+        )
+
+
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-
-from app.core.config import Settings
-
-settings = Settings()
 app = FastAPI()
 
 
 @app.get("/")
 def root():
-    return {"message": settings.postgres_db}
+    return {"message": "root"}
