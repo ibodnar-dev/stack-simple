2934cea0f4fd49e22fa03f0c9ffe1f6658a750d3
ivan <ibodnar.dev@gmail.com>
2024-12-26 07:09:42 -0300

    tf app runner service created

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -17,3 +17,37 @@ resource "aws_ecr_repository" "repo" {
   name                 = "stack-simple"
   image_tag_mutability = "MUTABLE"  # or IMMUTABLE
 }
+
+# App Runner service
+resource "aws_apprunner_service" "service" {
+  service_name = "stack-simple"
+
+  source_configuration {
+    authentication_configuration {
+      access_role_arn = "arn:aws:iam::977099018154:role/service-role/AppRunnerECRAccessRole"
+    }
+
+    image_repository {
+      image_identifier      = "${aws_ecr_repository.repo.repository_url}:latest"
+      image_repository_type = "ECR"
+
+      image_configuration {
+        port = "8000"
+        runtime_environment_variables = {
+          # Add any env vars your app needs
+          ENV = "prod"
+        }
+      }
+    }
+  }
+
+  instance_configuration {
+    cpu    = "1024"  # 1 vCPU
+    memory = "2048"  # 2 GB
+  }
+}
+
+# Output the service URL
+output "service_url" {
+  value = aws_apprunner_service.service.service_url
+}
